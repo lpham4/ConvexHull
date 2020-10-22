97a2b10aa9eca255ba28cff01e29a17fd6dca275
Ly Pham <lpham4@students.kennesaw.edu>
2020-10-22 19:14:02 -0400

    changed file

--- hull.py
+++ hull.py
@@ -1,7 +1,26 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial import ConvexHull
+import matplotlib.image as mpimg
 
-x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = [1,4,7,10,13,11,11,11,12,15,16,18,19,22]
+y = [6,15,7,13,6,18,21,10,18,6,3,12,15,19]
+points = np.array([(1,6),(4,15),(7,7),(10,13),(13,6),(12,18),(11,21),(11,10),(12,18),(15,6),(16,3),(18,12),(19,15),(22,19)])
 
+# random values for x and y values
+a = np.random.normal(0,10,20)
+b = np.random.normal(0,10,20)
+
+# calling the first hull with given points
 plt.scatter(x, y)
+hull1 = ConvexHull(points)
+for simplex in hull1.simplices:
+    plt.plot(points[simplex, 0], points[simplex, 1])
+plt.show()
+
+# calling the second hull with random generated points
+ab = np.hstack((a[:,np.newaxis],b[:,np.newaxis]))
+hull2 = ConvexHull(ab)
+plt.scatter(a,b)
+plt.plot(a[hull2.vertices], b[hull2.vertices])
 plt.show()
